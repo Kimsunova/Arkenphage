78f625b8f3c56b840db00e45e47f07cc87580010
Ripper Smith <rippersmith61@gmail.com>
2019-08-30 23:30:50 -0400

    Door Start
    Started implementng a door to go between scenes

---
+++ Arkenphage/Assets/DoorScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorScript : MonoBehaviour {
+
+    public string sceneToGoTo;
+    public Vector3 moveToPoint;
+    GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+
+    public void GoToScene()
+    {
+        SceneManager.LoadScene(sceneToGoTo, LoadSceneMode.Single);
+        player.transform.position = moveToPoint;
+    }
+}
